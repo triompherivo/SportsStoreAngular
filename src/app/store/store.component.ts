52f1d668033a3f65f31cd3b64dc82871dd9bdb25
Ramandalahy Triomphe <triompherivo@gmail.com>
2020-06-14 12:52:06 +0200

    Chapter 7 is finished ...

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -9,9 +9,13 @@ import {Product} from "../model/product.model";
 })
 export class StoreComponent {
   public selectedCategory = null;
+  public productsPerPage = 4;
+  public selectedPage = 1;
   constructor(private repository: ProductRepository) {}
   get Products() : Product[] {
-    return this.repository.getProducts(this.selectedCategory);
+    let pageIndex = (this.selectedPage - 1) * this.productsPerPage
+    return this.repository.getProducts(this.selectedCategory)
+      .slice(pageIndex,pageIndex + this.productsPerPage);
   }
 
   get Categories():string[]{
@@ -20,5 +24,25 @@ export class StoreComponent {
 
   changeCategory(newCategory?:string){
     this.selectedCategory = newCategory;
+  }
+
+  changePage(newPage:number){
+    this.selectedPage = newPage;
+  }
+  changePageSize(newSize:number){
+    this.productsPerPage = Number(newSize);
+    this.changePage(1);
+
+  }
+  get pageCount(): number {
+    return Math.ceil(this.repository
+      .getProducts(this.selectedCategory).length / this.productsPerPage)
+
+  }
+  get pageNumbers():Number[]{
+    return Array(Math.ceil(this.repository
+      .getProducts(this.selectedCategory).length / this.productsPerPage
+    ))
+      .fill(0).map((x,i) => i+1);
   }
 }
